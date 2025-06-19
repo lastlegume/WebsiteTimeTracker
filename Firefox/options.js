f377def7e2226df3d262683264cf957dff094240
lastlegume <lastlegume@gmail.com>
2025-06-19 01:44:35 -0500

    export function started

--- Firefox/options.js
+++ Firefox/options.js
@@ -9,7 +9,7 @@ function restoreOptions() {
   function setCurrentChoice(result) {
     document.querySelector("#numDisplayed").value = result.numDisplayed || 10;
     document.querySelector("#endTime").value = result.endTime || '00:00';
- //   console.log(result.numDisplayed);
+    //   console.log(result.numDisplayed);
   }
 
   function onError(error) {
@@ -23,3 +23,31 @@ function restoreOptions() {
 }
 document.addEventListener("DOMContentLoaded", restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
+
+document.getElementById("export").addEventListener("click", exportData);
+document.getElementById("import").addEventListener("click", importData);
+
+async function exportData() {
+  let storage = await browser.storage.local.get();
+  var file = new File([JSON.stringify(storage)], 'latex.txt', {
+    type: 'text/plain',
+  })
+  download();
+  function download() {
+    const link = document.createElement('a')
+    const url = URL.createObjectURL(file)
+
+    link.href = url
+    link.download = file.name
+    document.body.appendChild(link)
+    link.click()
+
+    document.body.removeChild(link)
+    window.URL.revokeObjectURL(url)
+  }
+}
+
+async function importData(){
+
+}
+
